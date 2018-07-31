81d643dec51634061ada5895297914aa951c11ba
Samuel Westen <Samuel.westen01@gmail.com>
2018-07-31 16:40:05 +1000

    refined version of same.js added

--- same.js
+++ same.js
@@ -1,3 +1,5 @@
+//My version, worked by lagged the system out.
+
 array1 = [58, 48, 91, 62];
 
 array2 = [3364, 2304, 8282, 3844];
@@ -25,3 +27,13 @@ function comp(array1, array2) {
 
 comp(a1, a2);
 
+//solution 
+
+function comp( array1, array2){
+    //If either of the arguments supplied are null
+    if(array1 == null || array2 == null) return false;
+    //assort the two arrys into ascending order
+    array1.sort((a, b) => a - b); array2.sort((a, b) => a - b)
+    //square each index of array 1 then test if each index is equal to array2 index
+    return array1.map(v => v * v).every((v, i ) => v == array2[i]);
+}
